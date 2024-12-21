3e2b7377f6e768659f9af3482d166c682f2e0862
vboxuser <vboxuser@ubuntu2.myguest.virtualbox.org>
2024-12-21 18:01:51 +0300

    We send message bit by bit using CWR flag parameterized encoding needs to be implemented Alper Yücel

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -1,4 +1,6 @@
 from CovertChannelBase import CovertChannelBase
+from scapy.all import IP, TCP, send, sniff
+import time
 
 class MyCovertChannel(CovertChannelBase):
     """
@@ -16,10 +18,39 @@ class MyCovertChannel(CovertChannelBase):
         - After the implementation, please rewrite this comment part to explain your code basically.
         """
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
+        for bit in binary_message:
+            cwr_flag = 0x80 if bit == '1' else 0x00  # CWR flag is the 8th bit in the TCP flags byte
+            pkt = IP(dst="receiver%eth0") / TCP( flags=cwr_flag)
+            print(f"bit: {bit}")
+            send(pkt, verbose=False)
+            time.sleep(30.0/1000)
         
+    
     def receive(self, parameter1, parameter2, parameter3, log_file_name):
         """
         - In this function, you are expected to receive and decode the transferred message. Because there are many types of covert channels, the receiver implementation depends on the chosen covert channel type, and you may not need to use the functions in CovertChannelBase.
         - After the implementation, please rewrite this comment part to explain your code basically.
         """
-        self.log_message("", log_file_name)
+        binary_message = ''
+        message=''
+        while True:
+            pkt = sniff(count=1,filter="tcp", lfilter=lambda p: TCP in p )[0]
+            if IP in pkt and TCP in pkt:
+                cwr_flag = pkt[TCP].flags & 0x80
+                bit = '1' if cwr_flag else '0'
+                print(f"bit: {bit}")
+            if bit is not None:
+                binary_message += bit
+                # End message when all bits of a byte are captured
+                if len(binary_message) % 8 == 0:
+                    char = chr(int(binary_message[-8:], 2))
+                    message += char
+                    print(f"Received char: {char}")
+                    if char == '.':  # end of message
+                        break
+        self.log_message(message, log_file_name)
+    
+    
+        
+        
+ 
